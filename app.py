563a0ed3ae234b76cb8df4b348cc750cd702bb6b
Soegidev <soegidev.id@gmail.com>
2022-05-01 15:24:36 +0700

    Add Fajar Testing

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, Docker!'
+@app.route('/test')
+def hello_test():
+    return 'Hello, Fajar Soegi!'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
