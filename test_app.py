6c38f49e949f143feda9517b9bd574105c048908
Soegidev <soegidev.id@gmail.com>
2022-05-01 22:59:42 +0700

    TEST 2

--- test_app.py
+++ test_app.py
@@ -30,5 +30,5 @@ def test_say(web, say):
 def test_say_age(web, say,age):
     response = web.get(f'/say/age/{say}/{age}')
     assert response.status_code == 200
-    assert response.data == b'Hello, Hai fajar soegi 14'
+    assert response.data == b'Hello, Hai fajar soegi 12'
     
