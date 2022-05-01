4aeb62a1d8973512b079b7918ce3e636c5dae88c
Soegidev <soegidev.id@gmail.com>
2022-05-01 21:10:01 +0700

    REMOVE TESTING BRANCH

--- app.py
+++ app.py
@@ -4,9 +4,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, Docker!'
-@app.route('/test')
-def hello_test():
-    return 'Hello, Fajar Soegi!'
+
+@app.route('/number/<int:number>')
+def hello_number(number):
+    return f'Hello,Your NUmber is {number} '
+
+@app.route('/say/<name>')
+def hello_say(name):
+    return f'Hello, {name}'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
